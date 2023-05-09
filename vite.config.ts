3196be856253c280265b20c3cdca916e26458c17
Arslan Mustafin <arslan.mustafin.dev@gmail.com>
2023-05-09 18:06:43 +0300

    init: start project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, './src/assets'),
+      components: path.resolve(__dirname, './src/components'),
+      pages: path.resolve(__dirname, './src/pages'),
+      routes: path.resolve(__dirname, './src/routes'),
+      services: path.resolve(__dirname, './src/services'),
+      types: path.resolve(__dirname, './src/types'),
+      utils: path.resolve(__dirname, './src/utils'),
+    },
+  },
+  plugins: [react({})],
+});
